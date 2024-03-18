0ac6d31e69027b8f221bf076a8cae304d62cc6a6
Ruan55 <114105714+Ruan55@users.noreply.github.com>
2024-03-18 10:48:44 -0300

    Add files via upload

---
+++ salario_minimo.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(){
+	setlocale(LC_ALL, "portuguese");
+	
+	// Declarando as variáveis
+	float salario, numeroDeSalarios, salarioMinimo = 1412.00;
+	
+	// Campo para o usuario preencher
+	printf("Digite o seu salario:\n");
+	scanf("%f", &salario);
+	
+	// Calculando o salario do usuario
+	numeroDeSalarios = salario / salarioMinimo;
+	
+	// Exibindo no terminal
+	printf("O usuario ganha: R$ %.2f\n", salario);
+	printf("Este usuario tem pelo menos: %.1f\n", numeroDeSalarios);
+	
+	
+}
