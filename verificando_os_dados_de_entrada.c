20c5f7140558f3ad7515c8148026ff6c04ef64d3
Ruan55 <114105714+Ruan55@users.noreply.github.com>
2024-03-19 10:32:39 -0300

    Add files via upload

---
+++ verificando_os_dados_de_entrada.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(){
+	setlocale(LC_ALL, "");
+	
+	// Declarando as variávaeis
+	int primeiroNumero, segundoNumero;
+	int soma, produto, menorValor, maiorValor;
+	float media;
+	
+	// Campo para o usuario preencher
+	printf("Digite o primeiro número:\n");
+	scanf("%d", &primeiroNumero);
+	printf("Digite o segundo número:\n");
+	scanf("%d", &segundoNumero);
+	
+	// Realizando os calculos
+	soma = primeiroNumero + segundoNumero;
+	media = soma / 2;
+	produto = primeiroNumero * segundoNumero;
+	
+	// Verificando qual é o maior e o menor valor
+	if(primeiroNumero > segundoNumero){
+		
+		printf("Primeiro Numero é maior\n");
+		
+	}
+	else{
+		
+		printf("Segundo numero é maior\n");
+		
+	}
+	
+	if(primeiroNumero < segundoNumero){
+		
+		printf("Primeiro numero é menor\n");
+		
+	}
+	else{
+		
+		printf("Segundo numero é menor\n");
+		
+	}
+	
+	// Verificando se os numero são iguais
+	if(primeiroNumero == segundoNumero){
+		
+		printf(" Os valores são iguais\n");
+		
+	}
+	else{
+		
+		printf(" Os valores são diferentes\n");
+		
+	}
+	
+	// Exibindo o resultado na tela
+	printf("Resultado:\n");
+	printf("Média: %.2f\n", media);
+	printf("Soma: %d\n", soma);
+	printf("Produto: %d\n", produto);
+
+}
