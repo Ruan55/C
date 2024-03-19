959993cf61d0a62845793bae96f1657982c03b86
Ruan55 <114105714+Ruan55@users.noreply.github.com>
2024-03-19 11:06:12 -0300

    Add files via upload

---
+++ verificacao_idade_voto.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(){
+	setlocale(LC_ALL, "");
+	
+	// Declarando as variáveis
+	char nome [50];
+	int idade;
+	
+	// Campo para o usuario preencher
+	printf("Digite seu nome:\n");
+	scanf("%s", &nome);
+	printf("Digite a sua idade:\n");
+	scanf("%d", &idade);
+	
+	if(idade < 18 || idade >= 65){
+		
+		printf("Não é obrigado a votar!\n");
+		
+	}
+	else{
+		
+		printf("É obrigado a votar!\n");
+		
+	}
+	
+}
