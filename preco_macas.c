512d3f1512dbbea259d57d0ea321298222374c43
Ruan55 <114105714+Ruan55@users.noreply.github.com>
2024-03-22 08:45:06 -0300

    Add files via upload

---
+++ preco_macas.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(){
+	setlocale(LC_ALL, "portuguese");
+	
+	// Declarando as variáveis
+	int maca;
+	float totalDaCompra;
+	
+	// Campo para o cliente preencher
+	printf("Digite a quantidade de maçãs compradas:\n");
+	scanf("%d", &maca);
+	
+	// Definindo o preço das maçãs
+	if(maca < 12){
+		
+		totalDaCompra = maca * 1.30;
+		
+	}
+	else if(maca >= 12){
+		
+		totalDaCompra = maca * 1.00;
+	}
+	
+	else{
+		
+		printf("Dados invalidos!\n");
+		
+	}
+	
+	// Exibindo os resultados na tela
+	printf("Resultado:\n");
+	printf("Maçãs compradas: %d\n", maca);
+	printf("Total da compra: R$ %.2f\n", totalDaCompra);
+	
+	
+	
+}
