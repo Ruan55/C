2471571d1a8fba386b8e13a61402d6dbde61c08f
Ruan55 <114105714+Ruan55@users.noreply.github.com>
2024-03-23 15:00:50 -0300

    Update atribuindo_o_desconto.c
    Adicionando comentários

--- atribuindo_o_desconto.c
+++ atribuindo_o_desconto.c
@@ -3,17 +3,21 @@
 #include <locale.h>
 
 int main(){
+	// Adicionando alguns elementos da lingua portuguesa
 	setlocale(LC_ALL, "portuguese");
 	
 	// Declarando as variáveis
 	float valor, desconto, valorFinal;
-	
+
+	// Campo para o cliente preencher
 	printf("Digite um valor:\n");
 	scanf("%f", &valor);
-	
+
+	// Calculando o desconto
 	desconto = valor * 0.10;
 	valorFinal = valor - desconto;
-	
+
+	// Imprimindo as informações na tela
 	printf("Valor: R$ %.2f\n", valor);
 	printf("Desconto: R$ %.2f\n", desconto);
 	printf("Valor final: R$ %.2f\n", valorFinal);
