74bcb21576aaebb28873c7e4f25ba36d3e443705
Ruan55 <114105714+Ruan55@users.noreply.github.com>
2024-03-23 15:13:02 -0300

    Update operacao_relacional.c
    Adicionando comentários

--- operacao_relacional.c
+++ operacao_relacional.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 
 int main(){
-	
+
+	// Declarando as variáveis
 	float primeiraNota, segundaNota, media;
-	
+
+	// Campo para o aluno preencher
 	printf("Digite a primeira nota:\n");
 	scanf("%f", &primeiraNota);
 	
 	printf("Digite a segunda nota:\n");
 	scanf("%f", &segundaNota);
-	
+
+	// Calculando a média
 	media = (primeiraNota + segundaNota)/2;
-	
+
+	// Verificando a média do aluno
 	if(media >= 7){
 		
 		printf("Parabens, voce esta aprovado! %.1f\n", media);	
