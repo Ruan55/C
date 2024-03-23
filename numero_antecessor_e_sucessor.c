7e768387382ab8bf7f7e6b0c816cd9786efbd564
Ruan55 <114105714+Ruan55@users.noreply.github.com>
2024-03-23 15:11:57 -0300

    Update numero_antecessor_e_sucessor.c
    Adicionando comentários

--- numero_antecessor_e_sucessor.c
+++ numero_antecessor_e_sucessor.c
@@ -3,16 +3,21 @@
 #include <locale.h>
 
 int main(){
+	// Adicionando alguns elementos da lingua portuguesa
 	setlocale(LC_ALL, "portuguese");
-	
+
+	// Declarando as variáveis
 	int numero, sucessor, antecessor;
-	
+
+	// Campo para o usuario preencher
 	printf("Digite um numero:\n");
 	scanf("%d", &numero);
-	
+
+	// Declarando os valores as variáveis dos numeros antecessores e sucessores
 	sucessor = numero + 1;
 	antecessor = numero - 1;
-	
+
+	// Exibindo o resultado no terminal
 	printf("Resultado:\n");
 	printf("Sucessor deste numero: %d\n", sucessor);
 	printf("Antecessor deste numero: %d\n", antecessor);
