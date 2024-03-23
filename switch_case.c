7cd9e3c2f747a73bbc7085c26c4765d3f2c233d5
Ruan55 <114105714+Ruan55@users.noreply.github.com>
2024-03-23 15:17:09 -0300

    Update switch_case.c
    Adicionando comentários

--- switch_case.c
+++ switch_case.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 int main(){
-	
+	// Declarando as variáveis
 	int diaUtil;
-	
+
+	// Campo para o usuario preencher
 	printf("Escolha entre dia util (1) ou fim de semana (2):\n");
 	scanf("%d", &diaUtil);
-	
+
+	// Programa para exibir na tela baseado na escolha do usuario
 	switch(diaUtil){
 		
 		case 1:
