3b4b21961543426c35aea87ec0c89f04cf651b26
Ruan55 <114105714+Ruan55@users.noreply.github.com>
2024-03-23 15:02:05 -0300

    Update break.c
    Adicionando comentários

--- break.c
+++ break.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
 int main(){
-	
+	// Declarando as variáveis
 	int i;
-	
+
+	// Aplicando a estrutura de repetição
 	for(i = 1; i <= 10; i++){
 		
 		printf("%d ", i);
 		
 		if(i == 9){
-			
+			// Comando para encerrar a estrutura de repetição
 			break;
 			
 		}
