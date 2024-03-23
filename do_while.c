532db4fc0faddc1b7a4949dc16730abb41adfe91
Ruan55 <114105714+Ruan55@users.noreply.github.com>
2024-03-23 15:07:40 -0300

    Update do_while.c
    Adicionando comentários

--- do_while.c
+++ do_while.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 int main(){
-	
+	// Declarando as variáveis
 	int i = 11;
-	
+
+	// Faz com que o bloco de comandos seja executado pelo menos uma vez de forma obrigatioria
 	do{
 		printf("%d ", i);
 		i++;
+	// Prosseguindo com o programa
 	}while(i <= 10);
 	
 }
