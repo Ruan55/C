7ac59431b54f9049867f70406f81c6561a9b9607
Ruan55 <114105714+Ruan55@users.noreply.github.com>
2024-03-23 15:05:35 -0300

    Update continue.c
    Adicionando comentários

--- continue.c
+++ continue.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 int main(){
-	
+	// Declarando as variáveis
 	int i;
-	
+
+	// Aplicando a estrutura de repetição
 	for(i = 1; i <= 10; i++){
 		
 		if(i == 5){
+			// Comando para pular o numero que está sendo comparado e logo em seguida prosseguir com o programa
 			continue;
 		}
 		printf("%d ", i);
