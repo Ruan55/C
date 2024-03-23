37555d1ec2241c1ba198528b97af5cd940896357
Ruan55 <114105714+Ruan55@users.noreply.github.com>
2024-03-23 14:58:51 -0300

    Update Operadores logicos.c
    Adicionando comentários

--- Operadores logicos.c
+++ Operadores logicos.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
 int main(){
-	
+
+	// Declarando as variáveis
 	float media;
-	
+
+	// Campo para o aluno preencher
 	printf("Insira a nota:\n");
 	scanf("%f", &media);
-	
+
+	// Verificando a média
 	if(media >= 4 && media < 7){
 		
 		printf("Tem direito a exame!\n");
