c3b9743f2cc2ccc2892700e536658f018a6260ad
Ruan55 <114105714+Ruan55@users.noreply.github.com>
2024-03-25 09:15:29 -0300

    Add files via upload

---
+++ numeros_impares.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+	// Declarando as variáveis
+	int i;
+	
+	// Verificando os numeros
+	for(i = 1; i <= 20; i++){
+		if(i % 2 != 0){
+			printf("%d\n", i);
+		}
+	}
+	
+}
---
+++ numeros_pares.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+	// Declarando as variáveis
+	int i;
+	
+	// Verificando os numeros
+		for(i = 100; i <= 120; i++){
+			if(i % 2 == 0){
+				printf("%d\n", i);
+			}
+		}
+
+	
+}
