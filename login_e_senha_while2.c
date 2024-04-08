ed02ad0533d3eba08257e736c5196f874793e662
Ruan55 <114105714+Ruan55@users.noreply.github.com>
2024-04-08 11:03:55 -0300

    Add files via upload

---
+++ login_e_senha_while2.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+
+int main(){
+	setlocale(LC_ALL, "portuguese");
+	
+	// Declarando as variáveis
+	int i;
+	char loginSalvo [500] = "Ruan";
+	char senhaSalvo [500] = "123456";
+	char login [500];
+	char senha [500];
+
+	// Aplicando a estrutura de repetição for
+	for(i = 0; i < 3; i++){
+	// Campo para o usuario preencher
+	printf("Digite o seu nome de usuario: ");
+	scanf("%s", &login);
+	
+	printf("Digite a seu senha: ");
+	scanf("%s", &senha);
+	
+	// Verificando as credenciais da conta
+	if(strcmp(login, loginSalvo) == 0 && strcmp(senha, senhaSalvo) == 0){
+		
+		printf("Acesso garantido!\n");
+		break;
+		
+	}else{
+		
+		printf("Acesso negado!\n");
+		
+	}
+	
+	}
+	
+}
