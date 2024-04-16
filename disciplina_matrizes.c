416a33202caefa59303dc4c0b407076ae80e2aa2
Ruan55 <114105714+Ruan55@users.noreply.github.com>
2024-04-16 11:19:23 -0300

    Add files via upload

---
+++ disciplina_matrizes.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main(){
+	setlocale(LC_ALL, "portuguese")	;
+	
+	char disciplina[3][200];
+	float notas[3][2];
+	float media[3];
+	int soma = 0;
+	int i, j;
+	
+	printf("Disciplinas:\n");
+	for(i = 0; i < 3; i++){
+		printf("Digite qual é a %dº disciplina: ", i + 1);
+		scanf("%s", &disciplina[i]);
+		
+		for(j = 0; j < 2; j++){
+			printf("Digite a sua %d nota: ", j + 1);
+			scanf("%f", &notas[i][j]);
+			
+			soma += notas[i][j];
+		}
+		
+		media[i] = soma / 2;
+		soma = 0;
+	}
+	
+	printf("\n");
+	
+	for(i = 0; i < 3; i++){
+		printf("%d Disciplina: %s\n", i + 1, disciplina[i]);
+		
+		for(j = 0; j < 2; j++){
+			printf("%d nota: %.2f\n", j + 1, notas[i][j]);
+		}
+		
+		printf("Sua média: %.2f\n", media[i]);
+		printf("\n");
+	}
+	
+}
