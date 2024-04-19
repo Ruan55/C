a89e63ef8e82db03d9fae8dcf2190a76f8353337
Ruan55 <114105714+Ruan55@users.noreply.github.com>
2024-04-19 07:46:21 -0300

    Update notas_alunos_matrizes.c
    Adicionando comentarios

--- notas_alunos_matrizes.c
+++ notas_alunos_matrizes.c
@@ -3,29 +3,35 @@
 
 int main(){
 	setlocale(LC_ALL, "portuguese");
-	
+
+	// Declarando as variáveis
 	char aluno[2][200];
 	float nota[2][3];
 	int i, j;
-	
+
 	printf("Digite suas notas:\n");
+	// Aplicando a estrutura de repetição for i
 	for(i = 0; i < 2; i++){
-		printf("Digite o nome do %d� aluno: ", i + 1);
+		// Campo para o usuario preencher
+		printf("Digite o nome do %dº aluno: ", i + 1);
 		scanf("%s", &aluno[i]);
-		
+
+		// Aplicando a estrutura de repetição for j
 		for(j = 0; j < 3; j++){
-			printf("Digite a %d� nota: ", j + 1);
+			// Campo para o usuario preencher
+			printf("Digite a %dº nota: ", j + 1);
 			scanf("%f", &nota[i][j]);
 		}
 	}
 	
 	printf("\n");
-	
+
+	// Exibindo o resultado no terminal
 	for(i = 0; i < 2; i++){
-		printf("%d� aluno: %s\n", i + 1, aluno[i]);
+		printf("%dº aluno: %s\n", i + 1, aluno[i]);
 		
 		for(j = 0; j < 3; j++){
-			printf("%d� nota: %.2f \n", j + 1, nota[i][j]);
+			printf("%dº nota: %.2f \n", j + 1, nota[i][j]);
 		}
 		
 		printf("\n");
