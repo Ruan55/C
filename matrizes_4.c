a7b9477edd414ef117ce53a84b7cd1fa58945cfa
Ruan55 <114105714+Ruan55@users.noreply.github.com>
2024-04-19 07:49:08 -0300

    Update matrizes_4.c
    Adicionar comentarios

--- matrizes_4.c
+++ matrizes_4.c
@@ -3,18 +3,23 @@
 
 int main(){
 	setlocale(LC_ALL, "portuguese");
-	
+
+	// Declarando as variáveis
 	int numeros[2][2];
 	int i, j;
 	
 	printf("Digite os elementos para matriz:\n");
+	// Aplicando a estrutura de repetição for i
 	for(i = 0; i < 2; i++){
+		// Aplicando a estrutura de repetição for j
 		for(j = 0; j < 2; j++){
+			// Campo para o usuario preencher
 			printf("Elemento de linha %d coluna %d ", i + 1, j + 1);
 			scanf("%d", &numeros[i][j]);
 		}
 	}
-	
+
+	// Exibindo o resultado no terminal
 	for(i = 0; i < 2; i++){
 		for(j = 0; j < 2; j++){
 			printf("Elemento da linha %d coluna %d: %d \n", i + 1, j + 1, numeros[i][j]);
