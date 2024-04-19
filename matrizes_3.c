631b44bc3148c0b03de211e798a5f254d42b8e24
Ruan55 <114105714+Ruan55@users.noreply.github.com>
2024-04-19 07:47:36 -0300

    Update matrizes_3.c
    Adicionando comentarios

--- matrizes_3.c
+++ matrizes_3.c
@@ -3,14 +3,17 @@
 
 int main(){
 	setlocale(LC_ALL, "portuguese");
-	
+
+	// Declarando as variáveis
 	int numeros[2][2];
-	
+
+	// Atribuindo os valores das matrizes
 	numeros[0][0] = 1;
 	numeros[0][1] = 2;
 	numeros[1][0] = 3;
 	numeros[1][1] = 4;
-	
+
+	// Imprimindo resultado no terminal
 	printf("Elemento 1: %d\n", numeros[0][0]);
 	printf("Elemento 2: %d\n", numeros[0][1]);
 	printf("Elemento 3: %d\n", numeros[1][0]);
